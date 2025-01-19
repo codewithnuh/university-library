665260f019d65a5a5e2f7e8b95f602d2cdec8617
codewithnuh <nuh25792@gmail.com>
2025-01-19 13:33:22 +0500

    initial commit

---
+++ app/(root)/page.tsx
@@ -0,0 +1,12 @@
+import { Button } from "@/components/ui/button";
+import React from "react";
+
+const HomePage = () => {
+  return (
+    <div>
+      <Button>Click ME</Button>
+    </div>
+  );
+};
+
+export default HomePage;
